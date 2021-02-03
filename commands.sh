8bb5aeabf9053541d71485eb925149a94ceae323
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-02-03 12:28:42 +0100

    Compile and run

--- commands.sh
+++ commands.sh
@@ -6,6 +6,17 @@ cd HiForest
 git clone -b 2011 git://github.com/cms-opendata-analyses/HiForestProducerTool.git HiForestProducer
 cd HiForestProducer
 
+scram b -j8
+
+if [ -z "$1" ]; then nev=100; else nev=$1; fi
+if [ -z "$2" ]; then config=hiforestanalyzer_cfg.py; else config=$2; fi
+# set the number of events
+eventline=$(grep maxEvents $config)
+sed -i "s/$eventline/process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32($nev) )/g" $config
+# remove the connection to cvmfs, for GT access from docker container  
+# sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" $config
+cmsRun $config
+
 echo  "### pwd ###"
 pwd
 echo  "### ls -l ###"
