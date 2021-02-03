45d2b686898f3c9ff7c10f85eb0b6ffc2f63cd1f
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-02-03 10:40:54 +0100

    Update minimal.yml (#1)
    * Update minimal.yml
    
    * Create commands.sh
    
    * Update minimal.yml

---
+++ commands.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -l
+echo  "### pwd ###"
+pwd
+echo  "### ls -l ###"
+ls -l
+echo  "### ls /cvmfs ###"
+ls /cvmfs
+echo "### ls /cvmfs/cms-opendata-conddb.cern.ch/ ###"
+ls /cvmfs/cms-opendata-conddb.cern.ch/
