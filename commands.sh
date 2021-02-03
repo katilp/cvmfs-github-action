b1f3da2ae8afb732de67bd00ebf725c5765f91dc
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-02-03 12:18:18 +0100

    Clone the HI code

--- commands.sh
+++ commands.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -l
 cmsrel CMSSW_4_4_7
+cd CMSSW_4_4_7/src
+mkdir HiForest
+cd HiForest
+git clone -b 2011 git://github.com/cms-opendata-analyses/HiForestProducerTool.git HiForestProducer
+cd HiForestProducer
+
 echo  "### pwd ###"
 pwd
 echo  "### ls -l ###"
 ls -l
-echo  "### ls /cvmfs ###"
-ls /cvmfs
 echo "### ls /cvmfs/cms-opendata-conddb.cern.ch/ ###"
 ls /cvmfs/cms-opendata-conddb.cern.ch/
