c7baa31ee5cdbc1ad04fc3f5caedc13b4f906e00
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-02-06 14:08:54 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -55,6 +55,10 @@ sed -i "s/$eventline/process.maxEvents = cms.untracked.PSet( input = cms.untrack
 # sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" $config
 cmsRun $config
 
+root -l -b forest2dimuon.C++
+
+cp *.png /mnt/vol/
+
 echo  "### pwd ###"
 pwd
 echo  "### ls -l ###"
